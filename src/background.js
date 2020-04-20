9df1e6fb030756857c99ddf88240739b43886a62
David Miller <david.miller@getg5.com>
2020-04-20 16:06:06 -0700

    updatables hooks

--- src/background.js
+++ src/background.js
@@ -1,1 +1,13 @@
-import store from './store'
+import axios from 'axios'
+// import store from './store'
+
+export default {
+  async getClients() {
+    const clients = await axios.get(updatablesApi.clients)
+    return clients.map(c => c.data.urn)
+  },
+  async getLocations(clientUrn) {
+    const locations = await axios.get(`${updatablesApi.locations}/${clientUrn}`)
+    return locations.map(l => l.data)
+  }
+}

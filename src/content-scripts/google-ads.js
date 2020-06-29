4ca661e1c04df9c93de6b41596144ab4c5a3507e
David Miller <david.miller@getg5.com>
2020-06-29 11:49:49 -0700

    more discriminating

--- src/content-scripts/google-ads.js
+++ src/content-scripts/google-ads.js
@@ -1,17 +1,19 @@
 (function() {
-  let codeAccount;
+  let codeAccount, customerId;
   const exId = document.querySelector('div.external-customer-id');
   const opId = document.querySelector('div.operating-customer > .id');
   if (!opId) {
     codeAccount = '';
+    // customerId = undefined;
   } else {
-    if (!exId || !exId.hasOwnProperty('innerText')) {
-      codeAccount = ''
+    if (exId) {
+      customerId = exId.innerText;
     }
     codeAccount = opId.innerText;
   }
+  console.log({ codeAccount, customerId });
   chrome.runtime.sendMessage({
     msg: 'google-ads',
-    data: { codeAccount, customerId: undefined }
+    data: { codeAccount, customerId }
   });
 })();
